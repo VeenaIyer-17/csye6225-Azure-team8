a1f24f29c8bac86f910ac3a66a765a43ae60545f
VeenaIyer-17 <iyer.v@northeastern.edu>
2019-12-02 16:33:21 -0500

    Databases s3 lambda and sns implemented

--- modules/network/base_network.tf
+++ modules/network/base_network.tf
@@ -1,15 +1,44 @@
 #Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name = "vnetvpc"
-    address_space = ["10.0.0.0/8"]
-    location = var.location1
+    name                = "vnetvpc"
+    address_space       = ["10.0.0.0/8"]
+    location            = var.location1
     resource_group_name = var.resource_group1
 }
 
 #Create subnet
 resource "azurerm_subnet" "subnet" {
     name = "LAN"
+    resource_group_name     = var.resource_group1
+    virtual_network_name    = azurerm_virtual_network.vnet.name
+    address_prefix          = "10.0.0.0/24"
+}
+
+resource "azurerm_route_table" "routeazure" {
+    name                = "projRouteTable"
+    location            = var.location1
     resource_group_name = var.resource_group1
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefix = "10.0.0.0/24"
+
+    route {
+        name            = "routeTable"
+        address_prefix  = "10.0.0.0/16"
+        next_hop_type   = "vnetlocal"
+    }
+
+    tags = {
+        environment     = "test"
+    }
+}
+
+resource "azurerm_route" "routetest" {
+    name                = "acceptanceTestRoute1"
+    resource_group_name = var.resource_group1
+    route_table_name    = "${azurerm_route_table.routeazure.name}"
+    address_prefix      = "10.0.0.0/24"
+    next_hop_type       = "vnetLocal"
+}
+
+resource "azurerm_subnet_route_table_association" "association1" {
+    subnet_id       = "${azurerm_subnet.subnet.id}"
+    route_table_id  = "${azurerm_route_table.routeazure.id}"
 }
--- modules/virtualmachine/vm_windows.tf
+++ modules/virtualmachine/vm_windows.tf
@@ -1,3 +1,15 @@
+resource "azurerm_public_ip" "publicIp" {
+    location = var.location2
+    name = "publicIp"
+    resource_group_name = var.resource_group2
+    allocation_method       = "Dynamic"
+    idle_timeout_in_minutes = 10
+
+    tags = {
+        environment = "prod"
+    }
+}
+
 resource "azurerm_network_interface" "windows_nic" {
     name = "NIC"
     location = var.location2
@@ -6,6 +18,7 @@ resource "azurerm_network_interface" "windows_nic" {
         name = "ipconfig"
         subnet_id = var.subnet_id
         private_ip_address_allocation = "Dynamic"
+        public_ip_address_id = azurerm_public_ip.publicIp.id
     }
 }
 
@@ -43,4 +56,103 @@ resource "azurerm_virtual_machine" "windows_vm" {
         timezone = "Romance Standard Time"
     }
  
+}
+
+resource "azurerm_mariadb_server" "mdbserver" {
+    name    = "mariadbserver"
+    location = var.location2
+    resource_group_name = var.resource_group2
+
+    sku {
+        name = "B_Gen5_2"
+        capacity = 2
+        tier = "Basic"
+        family = "Gen5"
+    } 
+
+    storage_profile {
+        storage_mb  = 51200
+        backup_retention_days = 7
+        geo_redundant_backup  = "Disabled"
+    }
+
+    administrator_login          = "csye6225azure"
+    administrator_login_password = "csye6225@Azure!"
+    version                      = "10.2"
+    ssl_enforcement              = "Enabled"
+}
+
+resource "azurerm_mariadb_database" "mdb" {
+    name                = "mariadb"
+    resource_group_name = var.resource_group2
+    server_name         = "${azurerm_mariadb_server.mdbserver.name}"
+    charset             = "utf8"
+    collation           = "utf8_general_ci"
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "dynamodb" {
+    name    = "cosmosdb-${random_integer.ri.result}"
+    location = var.location2
+    resource_group_name = var.resource_group2
+
+    offer_type          = "Standard"
+    kind                = "GlobalDocumentDB"
+
+    enable_automatic_failover = true
+
+    consistency_policy {
+        consistency_level       = "BoundedStaleness"
+        max_interval_in_seconds = 10
+        max_staleness_prefix    = 200
+    }
+
+    geo_location {
+        location          = var.location2
+        failover_priority = 0
+    }
+}
+
+resource "azurerm_storage_account" "storage" {
+    name                     = "csye6225storageazure"
+    resource_group_name      = var.resource_group2
+    location                 = var.location2
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storagecontainer" {
+    name                    = "csye6225content"
+    storage_account_name    = "${azurerm_storage_account.storage.name}"
+    container_access_type   = "private" 
+}
+
+resource "azurerm_storage_blob" "blobstorage" {
+    name                    = "index.zip"
+    storage_account_name    = "${azurerm_storage_account.storage.name}"
+    storage_container_name  = "${azurerm_storage_container.storagecontainer.name}"
+    type                    = "Block"
+}
+
+resource "azurerm_app_service_plan" "serviceplan" {
+    name                    = "azure-functions-test-service-plan"
+    location                =  var.location2
+    resource_group_name     = var.resource_group2
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_function_app" "lambdafunction" {
+    name                        = "azurefunction-${random_integer.ri.result}"
+    location                    =  var.location2
+    resource_group_name         =  var.resource_group2
+    app_service_plan_id         = "${azurerm_app_service_plan.serviceplan.id}"
+    storage_connection_string   = "${azurerm_storage_account.storage.primary_connection_string}"
 }
