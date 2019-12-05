653b76658ff92ade5fd883952511b16aea60be59
doijoy46 <doijodeharish.a@northeastern.edu>
2019-12-05 02:00:33 -0500

    Added Packer json and azure functions

---
+++ modules/Lambda/variables.tf
@@ -0,0 +1,27 @@
+variable "prefix" {
+    type = "string"
+    default = "team8"
+}
+
+variable "location" {
+    type = "string"
+    default = "westus2"
+}
+
+variable "environment" {
+    type = "string"
+    default = "dev"
+}
+
+variable "functionapp" {
+    type = "string"
+    default = "./Azure/HttpTrigger.zip"
+}
+
+resource "random_string" "storage_name" {
+    length = 24
+    upper = false
+    lower = true
+    number = true
+    special = false
+}
